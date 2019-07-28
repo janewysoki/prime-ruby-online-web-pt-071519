46ba8a66409cbec1d2b3f81ec9d48788ba1c6a64
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-28 17:06:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,8 @@ def prime?(integer)
   if integer < 2
     return false
   end
-  (2..integer-1).each do
+  (2..integer-1)
 end
+
+
+#test divisibility of number against a list of numbers ranging from 2 to the number less than the given number by 1 (i.e. if number is 35, check if 35 is divisible by any of the numbers from 2 to 34)
